037001876b3d34e1b9a89acdbbcace3af0228d53
Adriano Cosme <acosme@proteste.org.br>
2014-02-07 20:46:45 -0200

    add circular primes

--- 32-pandigital_products.py
+++ 32-pandigital_products.py
@@ -3,7 +3,7 @@ The product 7254 is unusual, as the identity, 39 x 186 = 7254, containing multip
 Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
 HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.'''
 
-
+#time: 2.5 hours
 
 def elements_has_nine_digits(product, multiplicand, multiplier):
     return len(str(multiplicand) + str(product) + str(multiplier)) == 9
---
+++ 35-circular_primes.py
@@ -0,0 +1,31 @@
+'''The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
+There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+How many circular primes are there below one million?'''
+
+import itertools
+
+def is_prime(i):
+    #print type(i)
+    if i <= 1: return False
+    if i == 2: return True
+    for n in range(2,i):
+        if i % n == 0: return False
+    return True
+
+def rotations_are_primes(num):
+    anagrams = ["".join(perm) for perm in itertools.permutations(str(num))]
+    for anagram in anagrams:
+        if not is_prime(int(anagram)):
+            return False
+    return True
+
+prime_list = []
+count_circulars = 0
+
+for i in range(1,1000000):
+    if is_prime(i):
+        if rotations_are_primes(i):
+            print "\n" + str(i)
+            count_circulars += 0
+
+
