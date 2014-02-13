68384c343ea3511d95792b1269a4a6ba6fbe2ddb
Adriano Cosme <acosme@proteste.org.br>
2014-02-13 10:48:02 -0200

    solved 35, circular primes

--- 35-circular_primes.py
+++ 35-circular_primes.py
@@ -5,27 +5,50 @@ How many circular primes are there below one million?'''
 import itertools
 
 def is_prime(i):
-    #print type(i)
     if i <= 1: return False
     if i == 2: return True
     for n in range(2,i):
-        if i % n == 0: return False
-    return True
-
-def rotations_are_primes(num):
-    anagrams = ["".join(perm) for perm in itertools.permutations(str(num))]
-    for anagram in anagrams:
-        if not is_prime(int(anagram)):
+        if i % n == 0:
             return False
     return True
 
-prime_list = []
-count_circulars = 0
+def rotations_are_primes(num):
 
-for i in range(1,1000000):
+    if not all_numbers_between_permitted(num): return False
+
+    rotations = []
+    rotation = str(num)
+    for i in range(1,len(str(num)) + 1):
+        rotation = rotation[1:] + rotation[0]
+        if not int(rotation) in rotations: rotations.insert(0,int(rotation))
+        if not is_prime(int(rotation)):
+            return False
+
+    circular_primes.extend(rotations)
+    return True
+
+
+
+
+def all_numbers_between_permitted(num):
+    if not len(str(num)) >= 2: return True
+    for char in str(num):
+        if not int(char) in [1,3,7,9]: return False
+    return True
+
+circular_primes = []
+count_circular = 0
+
+for i in range(0,1000000):
+#for i in range(0,10000):
+#for i in range(0,100):
     if is_prime(i):
-        if rotations_are_primes(i):
-            print "\n" + str(i)
-            count_circulars += 0
+        print i
+        if not i in circular_primes and rotations_are_primes(i):
+            count_circular += 1
+            print "\n->"
+            print circular_primes
 
-print count_circulars
+print circular_primes
+print "total => %d" % len(circular_primes)
+print "count first circulars => %d" % count_circular
